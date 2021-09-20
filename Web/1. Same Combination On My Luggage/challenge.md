eac100e673f4aa9241d6eb7348b187501a27143a
Jørgen Lund <jlu@dips.no>
2021-09-20 15:54:31 +0200

    Add Web 1

---
+++ Web/1. Same Combination On My Luggage/challenge.md
@@ -0,0 +1,5 @@
+We've just finished implementing our brand new password policy.
+
+Unfortunately, we've had to close account creation until the angry tech support calls have subsided, but that's a small price to pay for security!
+
+Can you log in as `admin` and find the flag?
---
+++ Web/1. Same Combination On My Luggage/luggage/luggage.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+func validateCredentials(username string, password string) bool {
+	if strings.ToLower(username) != "admin" {
+		return false
+	}
+
+	// Allow any variation of the passwords which contain at least one capital and lowercase letter:
+	if strings.ToUpper(password) == password {
+		return false
+	}
+
+	if strings.ToLower(password) == password {
+		return false
+	}
+
+	// https://www.pcmag.com/news/2020s-most-common-passwords-are-laughably-insecure
+	candidates := []string{"password1", "qqww1122", "aaron431", "million2"}
+	for _, candidate := range candidates {
+		if strings.ToLower(password) == candidate {
+			return true
+		}
+	}
+
+	return false
+}
+
+type Contents struct {
+	HintComments template.HTML
+}
+
+func main() {
+	body := Contents{
+		HintComments: template.HTML("<!-- <li>Can't be one of the top 20 most popular passwords</li>--><!-- TODO: Can someone explain why Admin doesn't want us to exclude all of the common passwords? --><!-- For some reason, Admin got very upset when I suggested excluding the top 20... -->"),
+	}
+
+	index := template.Must(template.ParseFiles("index.html"))
+	wrong := template.Must(template.ParseFiles("wrong.html"))
+	correct := template.Must(template.ParseFiles("correct.html"))
+	http.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {
+		valid := validateCredentials(r.FormValue("username"), r.FormValue("password"))
+		if valid {
+			correct.Execute(w, nil)
+			return
+		}
+
+		wrong.Execute(w, body)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		index.Execute(w, body)
+	})
+
+	http.ListenAndServe(":80", nil)
+}
